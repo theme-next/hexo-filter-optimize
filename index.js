e81ce207e070d5c030c482e8cedc8a5d119a79d2
Mimi <1119186082@qq.com>
2019-12-02 01:26:18 +0800

    Merge config

--- index.js
+++ index.js
@@ -3,20 +3,27 @@
 'use strict';
 
 var env = process.env.NODE_ENV;
-var filterOptimize = hexo.config.filter_optimize;
-if (env !== 'development' && filterOptimize && filterOptimize.enable) {
-  var filter = require('./lib/filter');
-
-  filterOptimize.css = filterOptimize.css || {};
-  filterOptimize.js = filterOptimize.js || {};
-
-  var priority = parseInt(filterOptimize.priority, 10);
-  if (isNaN(priority)) {
-    priority = 10;
+var filterOptimize = hexo.config.filter_optimize = Object.assign({
+  enable: true,
+  remove_comments: false,
+  css: {
+    bundle: true,
+    delivery: true,
+    inlines: ['css/main.css'],
+    excludes: []
+  },
+  js: {
+    bundle: true,
+    excludes: []
   }
+}, hexo.config.filter_optimize);
+
+if (env !== 'development' && filterOptimize.enable) {
+  var filter = require('./lib/filter');
+  var priority = parseInt(filterOptimize.priority, 10) || 10;
 
   // enable one of the optimizations
-  if (filterOptimize.css.enable || filterOptimize.js.bundle) {
+  if (filterOptimize.css.bundle || filterOptimize.js.bundle) {
     hexo.extend.filter.register('after_generate', filter, priority);
   }
 }
